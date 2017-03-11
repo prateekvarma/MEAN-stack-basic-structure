f9dc96217ccebef94de492789216b3f323d48b54
Prateek Varma <prtk.varma@gmail.com>
2017-03-11 00:16:55 +0000

    adding all files

---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,5 @@
+angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+
+    $scope.tagline = 'Over the hills from the NerdController!';
+
+});
---
+++ scraps/bowerScraps.js
@@ -0,0 +1,20 @@
+/*
+{
+  "name": "plural-mean",
+  "description": "Testing how the mean stack works",
+  "main": "index.js",
+  "authors": [
+    "Prateek Varma"
+  ],
+  "license": "ISC",
+  "homepage": "",
+  "private": true,
+  "dependencies": {
+    "jquery": "^3.1.1",
+    "toastr": "^2.1.3",
+    "angular": "^1.6.2",
+    "angular-resource": "^1.6.2",
+    "angular-route": "^1.6.2"
+  }
+}
+*/
